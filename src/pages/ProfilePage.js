13053a9fffe7ace53b73cc2bc310e62990f9f822
Lozano7 <lozanoj914@gmail.com>
2021-12-08 01:08:24 -0500

    Subiendo archivos

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom';
+
+const ProfilePage = () => {
+  const { username } = useParams();
+  return (
+    <div>
+      <h1>Bienvenido {username}</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -5,6 +5,7 @@ import AboutPages from '../pages/AboutPage';
 import ContactPage from '../pages/ContactPage';
 import HomePage from '../pages/HomePage';
 import NotFoundPage from '../pages/NotFoundPage';
+import ProfilePage from '../pages/ProfilePage';
 
 export default function AppRouter() {
   return (
@@ -13,6 +14,7 @@ export default function AppRouter() {
       <Switch>
         <Route path='/about' component={AboutPages} exact />
         <Route path='/contact' component={ContactPage} exact />
+        <Route path='/profile/:username' component={ProfilePage} exact />
         <Route path='/' component={HomePage} exact />
         <Route path='*' component={NotFoundPage} />
       </Switch>
